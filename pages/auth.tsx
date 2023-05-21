fd09777b2d052ff4415dbe79e204364c696c9568
Mayank Goel <goel.mayank235@gmail.com>
2023-05-21 23:45:15 +0530

    Added github signIN

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import { useCallback, useState } from 'react';
 import axios from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
@@ -55,16 +57,28 @@ const Auth = () => {
         </nav>
         <div className='flex justify-center'>
           <div className='bg-black bg-opacity-70 self-center px-16 py-16 mt-2 lg:w-2/5 lg:max-w-md rounded-md  w-full'>
-            <h2 className='text-white text-4xl mb-8 font-semibold'>{variant === 'login' ? 'Sign In' : 'Register'}</h2>
+            <h2 className='text-white text-4xl mb-8 font-semibold'>
+              {variant === 'login' ? 'Sign In' : 'Register'}
+            </h2>
             <div className='flex flex-col gap-4'>
               {variant === 'register' && (<Input label='Username' onChange={(ev: any) => { setName(ev.target.value) }} id='name' type='text' value={name} />)}
 
               <Input label='Email' id='email' type='email' value={email} onChange={(ev: any) => { setEmail(ev.target.value) }} />
               <Input label='Password' id='password' type='password' value={password} onChange={(ev: any) => { setPassword(ev.target.value) }} />
             </div>
-            <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'> {variant === 'login' ? "Log In" : "Sign Up"}</button>
+            <button onClick={variant === 'login' ? login : register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'> {variant === 'login' ? "Log In" : "Sign Up"}
+            </button>
+            <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+              <div className="w-10 h-10 bg-white rounded-full flex cursor-pointer hover:opacity-80 items-center justify-center transition">
+                <FcGoogle size={30}></FcGoogle>
+              </div>
+              <div className="w-10 h-10 bg-white rounded-full flex cursor-pointer hover:opacity-80 items-center justify-center transition">
+                <FaGithub size={30}></FaGithub>
+              </div>
+            </div>
             <p className='text-neutral-500 mt-12'>
               {variant === 'login' ? 'First time using Netflix?? ' : 'Already have an account?? '}
+
               <span onClick={toggleVariant} className='text-white hover:underline cursor-pointer'>
                 {variant === 'login' ? 'Create an Account ' : 'Log In '}
               </span>
