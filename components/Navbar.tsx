bf7ad006e31c9c29cbdcb4e87db7d6c54e3f37bd
yellowberard <goelmanu002@gmail.com>
2023-10-13 02:53:00 +0530

    added minor changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     return (
         <nav className="w-full fixed z-40 ">
             <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackgorund ? "bg-zinc-900 bg-opacity-90" : ''} `}>
-                <img className="h-5 lg:h-7" src="../images/logo.png" alt="logo" />
+                <img className="h-5 lg:h-16" src="../images/logo.png" alt="logo" />
                 <div className="flex-row ml-8 gap-7 hidden lg:flex">
                     <NavbarItem label='Home' />
                     <NavbarItem label='Series' />
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -58,7 +58,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
       <div className='bg-black w-full h-full lg:bg-opacity-50'>
         <nav className=' px-12 py-5'>
-          <img src='/images/logo.png' alt='logo' className='h-14' />
+          <img src='/images/logo.png' alt='logo' className='h-40' />
         </nav>
         <div className='flex justify-center'>
           <div className='bg-black bg-opacity-70 self-center px-16 py-16 mt-2 lg:w-2/5 lg:max-w-md rounded-md  w-full'>
@@ -88,7 +88,7 @@ const Auth = () => {
               </div>. */}
             </div>
             <p className='text-neutral-500 mt-12'>
-              {variant === 'login' ? 'First time using Netflix?? ' : 'Already have an account?? '}
+              {variant === 'login' ? 'First time using?? ' : 'Already have an account?? '}
 
               <span onClick={toggleVariant} className='text-white hover:underline cursor-pointer'>
                 {variant === 'login' ? 'Create an Account ' : 'Log In '}
